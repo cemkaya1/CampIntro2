a41bb7a66db9e50abd8e979652cbf0bae651d91a
Cem Kaya <cem62@hotmail.com>
2021-02-24 21:09:28 +0300

    Dictionary Home Work

---
+++ CollectionsMyDictionary/MyList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionsMyDictionary
+{
+    class MyList<K,V>
+    {
+        K[] key;
+        V[] value;
+        public MyList()
+        {
+            key = new K[0];
+            value = new V[0];
+        }
+        public void AddItems(K keyItem, V valueItem)
+        {
+            K[] tempKey = key;
+            V[] tempValue = value;
+            key = new K[key.Length + 1];
+            value = new V[value.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                key[i] = tempKey[i];
+                value[i] = tempValue[i];
+            }
+            //for (int i = 0; i < 2; i++)
+            //{
+            //    key[i] = tempKey[i];
+            //    value[i] = tempValue[i];
+            //}
+            key[key.Length - 1] = keyItem;
+            value[value.Length - 1] = valueItem;
+        }
+        public void ListItems()
+        {
+            for (int i = 0; i < key.Length; i++)
+            {
+                Console.WriteLine("Licance plate code: " + key[i] + ", " + "City Name: " + value[i]);
+            }
+        }
+        public int ItemsLenght
+        {
+            get { return key.Length; }
+        }
+    }
+}
---
+++ CollectionsMyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CollectionsMyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int, string> cities = new MyList<int, string>();
+            cities.AddItems(1, "Adana");
+            cities.AddItems(6, "Ankara");
+            cities.AddItems(19, "Corum");
+            cities.ListItems();
+            Console.WriteLine("Total items in the list is " + cities.ItemsLenght);
+        }
+    }
+}
